3bd45cbefd67c890dd032c73474cd39026f2ce20
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2022-11-02 00:39:15 +0600

    added

--- javascript/deal.js
+++ javascript/deal.js
@@ -56,11 +56,14 @@ fetch("./json/products.json")
 
     for(let product of products){
         const {special, frontPic, backPic, type, name, rating, provider, newPrice, prevPrice} = product;
+
       proAll += `
       
 
       <div class="popular-product-div display all">
-      <span class="upper-top hot">${special}</span>
+
+      <span class= "upper-top ${special === `-14%` ? `orange` : special.toLowerCase()}">${special}</span>
+
       <a href="#/" class="popular-img-box">
           <img src="${frontPic}" alt=${name} class="popular-front-img">
           <img src="${backPic}" class="popular-back-img">
@@ -112,5 +115,51 @@ fetch("./json/products.json")
       `
     }
 
-    snackProductContainer.innerHTML = proAll;
+    snackProductContainer.innerHTML = proAll
+})
+
+
+//Price range code starts from here......
+
+const rangeInput = document.querySelectorAll('.snack-range-input input'),
+priceInput = document.querySelectorAll('.snack-price-input input'),
+progress = document.querySelector('.snack-price-slider .snack-price-progress');
+
+let priceGap = 1000;
+
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(priceInput[0].value),
+        maxVal = parseInt(priceInput[1].value);
+
+        if((maxVal - minVal >= priceGap) && maxVal <= 10000){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            }else{
+                rangeInput[1].value = maxVal;
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    })
+})
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+
+        if(maxVal - minVal < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap;
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    })
 })
