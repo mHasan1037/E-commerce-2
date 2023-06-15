da7daa87ea285c886ab19b63f7cf8a70b9ab53cc
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-06-15 17:43:43 +0600

    added a new feature. if you click the quick view of every product. you will get the product detail in a model fetched from the api

--- javascript/homePage/quickView.js
+++ javascript/homePage/quickView.js
@@ -17,12 +17,52 @@ function fetchTarget(target){
     )
 }
 
+const qkViewModalContainerBody = document.querySelector('.qkView-modal-container-body')
+
 function getDataHere(datas, target){
-   const ans = datas.filter(data =>{
+   const filteredData = datas.filter(data =>{
         return data.id == target
      })
 
-     console.log(ans[0])
+     const {name, type, rating, newPrice, prevPrice, frontPic, backPic} = filteredData[0]
+
+     qkViewModalContainerBody.innerHTML = '';
+
+     const qkViewModalContainer = document.createElement('div')
+     qkViewModalContainer.classList.add('qkView-modal-container')
+
+     qkViewModalContainerBody.classList.add('show')
+     qkViewModalContainer.innerHTML = `
+            <i class="fa-solid fa-xmark qkView-modal-close"></i>
+            <div class="qkView-product-box">
+                <img src=${frontPic} class="quickPic"/>
+                <div>
+                    <h2>${name}</h2>
+                    <p>${type}</p>
+                    <p>Description:- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                    <p>Rating: ${rating}</p>
+                    <div style="display: flex; gap: 20px;">
+                        <p>Previous price: <span>${prevPrice}</span></p>
+                        <p>New price: <span>${newPrice}</span></p>
+                    </div>
+                    <button>Add to cart</button>
+                </div>
+            </div>
+     `
+
+     qkViewModalContainerBody.appendChild(qkViewModalContainer)
+
+     console.log(filteredData[0])
+
+     const quickPic = document.querySelector('.quickPic')
+
+     quickPic.addEventListener('mouseover', ()=> quickPic.src = backPic)
+     quickPic.addEventListener('mouseout', ()=> quickPic.src = frontPic)
+     
+     const qkViewModalClose = document.querySelector('.qkView-modal-close')
+     qkViewModalClose.addEventListener('click', ()=> {
+        qkViewModalContainerBody.classList.remove('show')
+     })
 }
 
 
