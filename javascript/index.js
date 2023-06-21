f26aa4db41ed4a52af947fbbd5976165ddcc6534
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-06-21 19:15:47 +0600

    testing

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-import { quickViewFunc } from 'quickView.js'
-import { bestSellsDisplay } from 'bestSellsDisplay.js'
+import { quickViewFunc } from './quickView.js'
+import { bestSellsDisplay } from './bestSellsDisplay.js'
 import { productAddCart } from './productOrder.js';
 
 //feature section slider starts from here......
@@ -283,7 +283,7 @@ dailyLists.forEach((dailyList, idx) =>{
     })
 })
 
-bestSellsDisplay(quickViewFunc)
+bestSellsDisplay()
 
 
 
